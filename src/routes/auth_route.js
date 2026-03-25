d390ee7e5e189e90cbf08027aa6f104bdef686c8
jcedeh <edeh.chinonso.james@gmail.com>
2026-03-25 22:06:52 +0100

    worked on verification link

--- src/routes/auth_route.js
+++ src/routes/auth_route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { sign_up_controller,
-         login_controller
+         login_controller,
+         activate_account_controller
  } from '../controllers/auth_controller.js';
 import { validateRequest } from '../middlewares/validation_middleware.js';
 import { login_schema, signup_schema } from '../validations/auth_validation.js';
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.post("/sign_up", validateRequest(signup_schema), sign_up_controller);
 router.post("/login", validateRequest(login_schema), login_controller);
+router.post("/verify-email", activate_account_controller);
 
 export default router;
